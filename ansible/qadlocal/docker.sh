41f96ddd387ca2e366698601590c4ca7082beb81
Alessandro Ventura <mysinmyc@users.noreply.github.com>
2019-01-27 22:36:28 +0100

    Update docker.sh

--- ansible/qadlocal/docker.sh
+++ ansible/qadlocal/docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+function list {
 cat <<EOF
 {
     "group": {
@@ -7,10 +8,10 @@ cat <<EOF
 EOF
 
 vOut=""
-for vCurContainerId in $(docker ps --format " {{.ID}}") 
+for vCurContainerId in $(docker ps --format " {{.ID}}")
 do
-	[ -n "${vOut}" ] && vOut="${vOut},"
-	vOut="${vOut}\"${vCurContainerId}\""
+        [ -n "${vOut}" ] && vOut="${vOut},"
+        vOut="${vOut}\"${vCurContainerId}\""
 done
 
 echo $vOut
@@ -23,3 +24,16 @@ cat <<EOF
     }
 }
 EOF
+}
+
+case $1 in
+        --list)
+                list
+        ;;
+        --host)
+        	docker inspect $2 -f "{{json .Config}} "
+        ;;
+        *)
+                exit 10
+        ;;
+esac
